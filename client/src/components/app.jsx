73e1ad4a522299e314ab9f7708e91decc5dabb73
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-12 17:25:19 -0600

    removed gutters and makes navbar flush with screen edges

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -15,7 +15,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
+      <div className="">
         <TitleBar />
         <Player />
       </div>
