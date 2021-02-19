7eef05d0c1fe2743a2fd3f0e40212e9bbe51ae3a
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-19 09:45:45 -0600

    Button Counter Works

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -5,18 +5,28 @@ import NavBar from "./NavBar/navBar";
 
 import Player from "./Player/player"
 
+import Button from "./Buttons/buttons"
+
 //Class or Stateful Component
 export default class App extends Component {
   constructor(props) {
     super(props);
-    // NOT USESLESS
+    this.state = {clickCount: 0};
+    this.onClickMSG = this.onClickMSG.bind(this);
+
+  }
+
+  onClickMSG(){
+    this.setState = this.state.clickCount++;
+    console.log(this.state.clickCount, "I've been click captain!")
   }
 
   render() {
     return (
       <div className="">
         <NavBar />
-        <Player />
+        {/* <Player /> */}
+        <Button className="red" children="test" onClick={this.onClickMSG}/>
       </div>
     );
   }
