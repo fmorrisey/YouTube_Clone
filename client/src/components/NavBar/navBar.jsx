64af92bc7ac21da56956f4aa6ea724c0ea776d54
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-27 15:10:16 -0600

    Rough draft of search bar implemented

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./navBar.css";
 import brand from "./yt_logo_rgb_dark.png";
 
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 
 // Top App NavBar
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import InputBase from "@material-ui/core/InputBase";
+
 
 // Side Drawer
 import clsx from "clsx";
@@ -33,6 +35,7 @@ import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -81,6 +84,45 @@ const useStyles = makeStyles((theme) => ({
   MuiDivider: {
     backgroundColor: "#373737",
     margin: theme.spacing(1),
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(1, 1),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 0, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+    backgroundColor: "#202020"
+    
   },
 }));
 
@@ -224,6 +266,30 @@ export default function NavBar() {
                 <MenuIcon />
               </IconButton>
               <img src={brand} alt={brand} className={classes.brand} />
+              <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              
+            </div>
+            <InputBase
+              placeholder="Search"
+              variant="outlined"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+            
+          </div>
+          <IconButton
+                
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <SearchIcon />
+              </IconButton>
             </Toolbar>
           </AppBar>
 
