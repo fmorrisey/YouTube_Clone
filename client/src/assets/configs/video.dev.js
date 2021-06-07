2ff38dc81535a69f549fcdd6e5823ab31afaa8ac
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-06-07 12:37:34 -0500

    Added config files to set up compoent props for future api intergration

---
+++ client/src/assets/configs/video.dev.js
@@ -0,0 +1,8 @@
+const currentVideo = {
+  videoURL: "https://www.youtube.com/watch?v=i793Qm6kv3U",
+  title: "Understanding React's UI Rendering Process",
+  views: 18142,
+  date: "Nov 5, 2019",
+};
+
+module.exports = currentVideo;
